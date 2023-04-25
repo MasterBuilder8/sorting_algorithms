e25a5b27464fac52c5bb4af6d4089a510d6fd23d
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-04-25 13:39:50 -0700

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,11 @@
 #include "sort.h"
-
-
 /**
- * quick_sort - function that sorts an array of integers in ascending order
- * using the Quick sort algorithm
- * @array: array of integers to be sorted
- * @size: size of array of integers to be sorted
+ *quick_sort - sorts an array of integers from smallest to largest
+ *@array: array to sort
+ *@size: size of array
  *
- * Return: nothing
+ *
+ *Return: no return
  */
 void quick_sort(int *array, size_t size)
 {
@@ -19,58 +17,58 @@ void quick_sort(int *array, size_t size)
 	quick_sort2(array, start, end, size);
 }
 /**
- * quick_sort2 - function that sorts an array of integers
- * @array: array of integers to be sorted
- * @start: the start of the array
- * @end: end of array to be sorted
- * @size: size  of array of integers  to be sorted
- * Return: nothing
+ *quick_sort2 - function sorts array of integers
+ *@array: array of ints
+ *@start: start of array
+ *@end: end of array/last index
+ *@size: size of array
+ *Return: nothing
  */
 void quick_sort2(int *array, int start, int end, int size)
 {
-	int pivot_index;
+	int pivot_idx;
 
 	if (start < end)
 	{
-		pivot_index = lomut_partition(array, start, end, size);
-		quick_sort2(array, start, pivot_index - 1, size);
-		quick_sort2(array, pivot_index + 1, end, size);
+		pivot_idx = lomut_partition(array, start, end, size);
+		quick_sort2(array, start, pivot_idx - 1, size);
+		quick_sort2(array, pivot_idx + 1, end, size);
 	}
 }
+
 /**
- * lomut_partition - function that partitions the array of integers
- * @array: array of the integers  to be sorted
- * @strt: first index
- * @nd: last index
- * @size: size of the array of integers to be sorted
- * Return: the index of the pivot element
+ *lomut_partition - partitions the main array
+ *@array: the array
+ *@strt: first index
+ *@nd: last index
+ *@size: size of array
+ *Return: the index of the pivot element
  */
 int lomut_partition(int *array, int strt, int nd, int size)
 {
-	int pivot = array[nd], pivot_index = strt, j, temp;
+	int pivot = array[nd], p_idx = strt, j, temp;
 
 	for (j = strt; j < nd; j++)
 	{
 		if (array[j] <= pivot)
 		{
-			if (pivot_index != j)
+			if (p_idx != j)
 			{
-				temp = array[pivot_index];
-				array[pivot_index] = array[j];
+				temp = array[p_idx];
+				array[p_idx] = array[j];
 				array[j] = temp;
 				print_array(array, size);
 			}
-			pivot_index++;
+			p_idx++;
 		}
 	}
-	if (pivot_index != nd)
+	if (p_idx != nd)
 	{
-		temp = array[pivot_index];
-		array[pivot_index] = array[nd];
+		temp = array[p_idx];
+		array[p_idx] = array[nd];
 		array[nd] = temp;
 		print_array(array, size);
 	}
 
-	return (pivot_index);
+	return (p_idx);
 }
-
